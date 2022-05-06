9b4a837eb4f06b884b762a717d0ec05a6afaea36
Manu <manugeo13@gmail.com>
2022-05-06 12:23:15 +0530

    Added nodemon and morgan.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hey there!");
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(express.json());
+morgan.token('content', (req, res) =>  JSON.stringify(req.body || null));
+morgan.token("custom", ":method :url :status :res[content-length] - :response-time ms :content");
+app.use(morgan('custom'));
+
+app.get('/', (req, res) => {
+  res.send("Hey there! This is Back-end 101.");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log("App running on port 3001"));
